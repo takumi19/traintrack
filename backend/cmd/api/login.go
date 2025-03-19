03a96d73bbbe4df48ec102fbeb8c5736b560b823
takumi19 <terrafin03@gmail.com>
2025-03-19 16:52:00 +0300

    feat: add login and signup routes

---
+++ backend/cmd/api/login.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"traintrack/internal/database"
+	"traintrack/internal/password"
+)
+
+func (a *Api) handleLogin(w http.ResponseWriter, r *http.Request) {
+	// Validate the password from the data in the database
+	// If not valid return unauthorized
+	// If valid, creates a JWT token and sends it back
+
+	var data signupRequest
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		a.l.Level(ERROR).Print("Failed to decode JSON from the request:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+  ///
+  if err := data.validate(); err != nil {
+    a.l.Level(ERROR).Println(err)
+		WriteJSON(w, http.StatusBadRequest, ApiError{Error: "missing fields"})
+		return
+	}
+
+	encryptedPassword, err := password.Hash(*data.PlaintextPassword)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ApiError{Error: "Hashing problems"})
+		return
+	}
+
+	user := &database.User{
+		FullName:     data.FullName,
+		Login:        data.Login,
+		Email:        data.Email,
+		PasswordHash: &encryptedPassword,
+	}
+
+  ///
+
+	existingUser, err := a.db.GetUserByEmail(*data.Email)
+	if err != nil {
+		if errors.Is(err, database.ErrNotFound) {
+			WriteJSON(w, http.StatusNotFound, &ApiError{Error: "email was not found"})
+			return
+		}
+		WriteJSON(w, http.StatusInternalServerError, &ApiError{Error: "internal"})
+		return
+	}
+
+	matches, err := password.Matches(*data.PlaintextPassword, *existingUser.PasswordHash)
+	if err != nil {
+		a.l.Level(ERROR).Println("Failed to hash the password")
+		WriteJSON(w, http.StatusInternalServerError, &ApiError{Error: "internal"})
+		return
+	}
+
+	if !matches {
+		WriteJSON(w, http.StatusUnauthorized, &ApiError{Error: "Wrong password"})
+		return
+	}
+
+	// Create JWT
+	token, err := createJWT(user)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ApiError{Error: "Failed to create token"})
+		return
+	}
+
+	WriteJSON(w, http.StatusOK, map[string]string{
+		"token": token,
+	})
+}
---
+++ backend/cmd/api/signup.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"traintrack/internal/database"
+	"traintrack/internal/password"
+)
+
+type signupRequest struct {
+	FullName          *string `json:"full_name" db:"full_name"`
+	Login             *string `json:"login" db:"login"`
+	Email             *string `json:"email" db:"email"`
+	PlaintextPassword *string `json:"password" db:"password"`
+}
+
+func (a *Api) handleSignup(w http.ResponseWriter, r *http.Request) {
+	var data signupRequest
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		a.l.Level(ERROR).Print("Failed to decode JSON from the request:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+  if err := data.validate(); err != nil {
+    a.l.Level(ERROR).Println(err)
+		WriteJSON(w, http.StatusBadRequest, ApiError{Error: "missing fields"})
+		return
+	}
+
+	encryptedPassword, err := password.Hash(*data.PlaintextPassword)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ApiError{Error: "Hashing problems"})
+		return
+	}
+
+	user := &database.User{
+		FullName:     data.FullName,
+		Login:        data.Login,
+		Email:        data.Email,
+		PasswordHash: &encryptedPassword,
+	}
+
+	_, err = a.db.CreateUser(user)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ApiError{Error: "Failed to create user"})
+		return
+	}
+
+	token, err := createJWT(user)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ApiError{Error: "Failed to create token"})
+		return
+	}
+
+	WriteJSON(w, http.StatusCreated, map[string]string{
+		"token": token,
+	})
+}
+
+func (data *signupRequest) validate() error {
+	if data.Email == nil {
+		return errors.New("no email")
+	}
+	if data.FullName == nil {
+		return errors.New("no full name")
+	}
+	if data.Login == nil {
+		return errors.New("no login")
+	}
+	if data.PlaintextPassword == nil {
+		return errors.New("no password")
+	}
+	return nil
+}
